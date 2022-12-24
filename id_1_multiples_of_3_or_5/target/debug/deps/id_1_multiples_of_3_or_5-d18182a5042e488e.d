70af52d6abe974ce9f44976915401327b31194bd
Aleaddin Ozer <ozeraleaddin@gmail.com>
2022-12-24 18:16:17 +0300

    update

--- id_1_multiples_of_3_or_5/target/debug/deps/id_1_multiples_of_3_or_5-d18182a5042e488e.d
+++
@@ -1,5 +0,0 @@
-/home/odin/Documents/GitHubProjects/projecteuler/inrust/id_1_multiples_of_3_or_5/target/debug/deps/id_1_multiples_of_3_or_5-d18182a5042e488e: src/main.rs
-
-/home/odin/Documents/GitHubProjects/projecteuler/inrust/id_1_multiples_of_3_or_5/target/debug/deps/id_1_multiples_of_3_or_5-d18182a5042e488e.d: src/main.rs
-
-src/main.rs:
